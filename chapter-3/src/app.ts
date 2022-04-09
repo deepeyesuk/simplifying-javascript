66295a3490d1c93d13a0e9c22a1005356c7d2e9d
hyungkyu <hyungkyu@deepeyes.co.uk>
2022-04-09 23:37:30 +0100

    feat: test code for tip13

--- chapter-3/src/app.ts
+++ chapter-3/src/app.ts
@@ -10,6 +10,8 @@
 //     return updated
 // }
 
+
+console.log('Tip 11 : Get started -----------')
 const defaults = { 
     author: '', 
     title: '',
@@ -27,13 +29,14 @@ const anotherBook = {
     year: 2016,
 }
 
+// console.log(defaults)
 // console.log(Object.assign(defaults, book))
 // console.log(Object.assign(defaults, anotherBook))
-// console.log(defaults)
+
 console.log('Tip 11 : Object.assing -----------')
 const book1 = Object.assign({}, defaults, book)
-// const book2 = Object.assign({}, defaults, anotherBook)
-const book2 = Object.assign({}, defaults, book1, anotherBook)
+const book2 = Object.assign({}, defaults, anotherBook)
+// const book2 = Object.assign({}, defaults, book1, anotherBook)
 console.log(book1, book2, defaults)
 
 console.log('Tip 11 : deep copying -----------')
@@ -75,3 +78,44 @@ console.log(defaultEmployee)
 employee3.name.first = 'Joe';
 console.log(employee3)
 console.log(defaultEmployee)
+
+console.log('Tip 13 : Map = dictionary -----------')
+let filters1 = new Map()
+    .set('breed', 'labrador')
+    .set('size', 'large')
+    .set('color', 'chocolate')
+
+console.log(filters1.get('size')) // 'large'
+
+let filters2 = new Map( 
+    [
+        ['breed', 'labrador'], 
+        ['size', 'large'], 
+        ['color', 'chocolate'],
+    ]
+)
+    
+console.log(filters2.get('color'))  // 'chocolate'
+
+filters2.delete('color'); 
+console.log(filters2.get('color'))
+filters2.clear()
+
+const errors1 = {
+    100: 'Invalid name',
+    110: 'Name should only contain letters', 
+    200: 'Invalid color'
+}
+
+console.log(Object.keys(errors1)) // ['100', '110', '200']
+
+let errors2 = new Map(
+    [
+        [100, 'Invalid name'],
+        [110, 'Name should only contain letters'], 
+        [200, 'Invalid color']
+    ]
+)
+console.log(errors2.get(100))
+
+console.log(errors2.keys()) // MapIterator { 100, 110, 200 } }
