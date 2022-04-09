7b9b05e8c0c83ad7d04afc3974c40968368ad55b
hyungkyu <hyungkyu@deepeyes.co.uk>
2022-04-09 20:55:32 +0100

    feat: test code for Tip12 added

--- chapter-3/src/app.ts
+++ chapter-3/src/app.ts
@@ -30,13 +30,13 @@ const anotherBook = {
 // console.log(Object.assign(defaults, book))
 // console.log(Object.assign(defaults, anotherBook))
 // console.log(defaults)
-console.log('-----------')
+console.log('Tip 11 : Object.assing -----------')
 const book1 = Object.assign({}, defaults, book)
 // const book2 = Object.assign({}, defaults, anotherBook)
 const book2 = Object.assign({}, defaults, book1, anotherBook)
 console.log(book1, book2, defaults)
 
-console.log('-----------')
+console.log('Tip 11 : deep copying -----------')
 const defaultEmployee = { 
     name: {
         first: '',
@@ -49,16 +49,29 @@ const employee = Object.assign({}, defaultEmployee)
 console.log(employee)
 console.log(defaultEmployee)
 
-employee.name.first = 'Joe';
-console.log(employee)
-console.log(defaultEmployee)
+// employee.name.first = 'Joe';
+// console.log(employee)
+// console.log(defaultEmployee)
 
-const employee2 = Object.assign( {},
-    defaultEmployee, 
-    {
-        name: Object.assign( {}, defaultEmployee.name )
-    }
-)
-console.log(employee)
-console.log(employee2)
+// const employee2 = Object.assign( {},
+//     defaultEmployee, 
+//     {
+//         name: Object.assign( {}, defaultEmployee.name )
+//     }
+// )
+// console.log(employee)
+// console.log(employee2)
+// console.log(defaultEmployee)
+
+console.log('Tip 12 : spread op -----------')
+console.log(defaults)
+const book3 = {...defaults, ...book}
+
+console.log(book3, defaults)
+
+const employee3 = {...defaultEmployee, name: {...defaultEmployee.name}}
+console.log(employee3)
+console.log(defaultEmployee)
+employee3.name.first = 'Joe';
+console.log(employee3)
 console.log(defaultEmployee)
