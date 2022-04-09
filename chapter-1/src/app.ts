b16047a49c8b1df25e4c9a9b5b06114d67a61082
hyungkyu <hyungkyu@deepeyes.co.uk>
2022-04-09 19:44:17 +0100

    feat: example code on ch1

---
+++ chapter-1/src/app.ts
@@ -0,0 +1,53 @@
+/* EXERCISE 4
+   TODO: Declare the array as the type to match the type of the items in the array. */
+
+//    let randomNumbers: Array<number> = [];
+//    let nextNumber;
+//    for (let i = 0; i < 10; i++) {
+//        nextNumber = Math.floor(Math.random() * (100 - 1)) + 1;
+//        randomNumbers.push(nextNumber);
+//    }
+   
+//    console.log(randomNumbers);
+
+// function addClick(items: any) { 
+//     console.log(items.length)
+//     for (var i = 0; i < items.length; i++) { 
+//         items[i].onClick = function () { 
+//             return i; 
+//         }; 
+//     } 
+//     return items; 
+// } 
+
+// const example = [{}, {}]
+// const clickSet = addClick(example) 
+// console.log(clickSet[0].onClick())
+// console.log(clickSet[1].onClick())
+
+
+// function addClick(items: any[]) {
+//     console.log(items.length)
+//     for (var i = 0; i < items.length; i++) {
+//         items[i].onClick = (function (i) { 
+//             return function () {
+//                 return i; 
+//             };
+//         }(i));
+//     }
+//     return items;
+// }
+
+function addClick(items: any[]) {
+    for (let i = 0; i < items.length; i++) {
+        items[i].onClick = function () { 
+            return i; 
+        }; 
+    }
+    return items; 
+}
+    
+const example = [{}, {}]
+const clickSet = addClick(example)
+console.log(clickSet[0].onClick())
+console.log(clickSet[1].onClick())
