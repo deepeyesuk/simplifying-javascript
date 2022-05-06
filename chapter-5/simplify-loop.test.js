9bef9f6ce5a20625ad261142031fa9d8cb8a77db
hyungkyu <hyungkyu@deepeyes.co.uk>
2022-05-06 21:02:32 +0100

    add test

--- chapter-5/simplify-loop.test.js
+++ chapter-5/simplify-loop.test.js
@@ -1,4 +1,6 @@
-it('should list all color of dogs', () => {
+const { filter } = require("lodash");
+
+describe('Reduce Test with Dogs', () => {
   const dogs = [ 
     {     
       name: 'max',
@@ -19,13 +21,39 @@ it('should list all color of dogs', () => {
       color: 'chocolate',
     },
   ]
+  
+  it('should list all color of dogs', () => {
+    const colors = dogs.reduce((colors, dog) => {
+      if (colors.includes(dog.color)) {
+        return colors;
+      }
+      return [...colors, dog.color]
+    }, [])
+  
+    expect(colors).toEqual(['black', 'chocolate'])
+  })
+  
+  it('should form a different shape from the original data', () => {
 
-  const colors = dogs.reduce((colors, dog) => {
-    if (colors.includes(dog.color)) {
-      return colors;
+    const expectedResult = {
+      bread: ['boston terrier', 'labrador', 'labrador'],
+      size: ['small', 'large', 'medium'],
+      color: ['black', 'black', 'chocolate']
     }
-    return [...colors, dog.color]
-  }, [])
 
-  expect(colors).toEqual(['black', 'chocolate'])
+    const filters = dogs.reduce((filters, dog) => {
+      filters.breed.add(dog.breed)
+      filters.size.add(dog.size)
+      filters.color.add(dog.color)
+      return filters
+    }, {
+      bread: new Set(),
+      size: new Set(),
+      color: new Set()
+    })
+
+    expect(filters).toEqual(expectedResult)
+  })
 })
+
+
